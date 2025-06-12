82ac2827ee98749545a265c8473bf856b6d52abb
feige838 <qcd838@gmail.com>
2025-06-13 00:05:27 +0800

    Site updated: 2025-06-13 00:05:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/08/hello-world/"];function toRandomPost(){
+var posts=["2025/06/12/Sofi支票账户开户/","2025/06/08/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
